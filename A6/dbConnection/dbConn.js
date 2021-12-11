844490cf5da10aac943b753cf8221edf4a612233
sdthaker <71615163+sdthaker@users.noreply.github.com>
2021-12-11 18:31:41 -0500

    Update dbConn.js

--- A6/dbConnection/dbConn.js
+++ A6/dbConnection/dbConn.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 //setup db connection
-var sequelize = new Sequelize('ddhrk5jjjqjoj2', 'ywbndchyfpblbk', 'f973eeed708ad80cc33a9a3a747eb9e02a23e8a4bcbd376e03e2fa7bd5a7694f', {
-    host: 'ec2-34-195-69-118.compute-1.amazonaws.com',
+var sequelize = new Sequelize(DB, USER, PASSWORD, {
+    host: HOST,
     dialect: 'postgres',
     port: 5432,
     dialectOptions: {
